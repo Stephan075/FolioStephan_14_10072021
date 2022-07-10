24cce582914b9c5006cc104f45e5d8d19c5efb8b
Stephan <stephan.folioo@gmail.com>
2022-07-10 14:38:53 +0200

    add first components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import Content from "./components/Content/Content";
+import Header from "./components/Header/Header";
+import styles from "./App.module.scss";
+
 function App() {
-  return <h1>HRnet</h1>;
+  return (
+    <div className={`${styles.appContainer} d-flex flex-column`}>
+      <Header />
+      <Content />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Content/Content.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Content = () => {
+  return <div className="flex-fill b3">Content</div>;
+};
+
+export default Content;
